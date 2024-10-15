ded6278c950332613a33d36876ccfe11c893c805
tito060804 <144065294+tito060804@users.noreply.github.com>
2024-10-15 15:01:29 -0300

    Add files via upload

---
+++ tpe-web2-vinoteca/router.php
@@ -0,0 +1,102 @@
+<?php
+require_once 'config.php';
+require_once './app/controllers/vinoteca.controller.php';
+require_once './app/controllers/auth.controller.php';
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$action = 'home';
+
+if (!empty( $_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+$params = explode('/', $action);
+
+switch ($params[0]) { 
+    case 'home':
+        $controller= new VinotecaController();
+        $controller->showHome();
+        break;
+    case 'listar':
+        $controller= new VinotecaController();
+        $controller->showVinos(); 
+        break;
+    case 'mostrarVino':
+        $controller= new VinotecaController();
+        $controller->showVino($params[1]); 
+        break;
+    case 'mostrarCepa':
+        $controller= new VinotecaController();
+        $controller->showCepa($params[1]); 
+        break;
+    case 'mostrarCepas':
+        $controller= new VinotecaController();
+        $controller->showCepas(); 
+        break;
+    case 'buscarVinosPorCepa':
+        $controller= new VinotecaController();
+        $controller->showBuscarPorCepa(); 
+        break;
+    case 'mostrarVinosPorCepa':
+        $controller= new VinotecaController();
+        $controller->showVinosPorCepa($params[1]); 
+        break;
+    case 'eliminarVino':
+        $controller= new VinotecaController();
+        $controller->eliminarVino($params[1]); 
+        break;
+    case 'modificarVino':
+        $controller= new VinotecaController();
+        $controller->showModificarVino($params[1]); 
+        break;
+    case 'enviarModificarVino':
+        $controller= new VinotecaController();
+        $controller->modificarVino($params[1]); 
+        break;
+    case 'agregarVino':
+        $controller= new VinotecaController();
+        $controller->showAgregarVino(); 
+        break;
+    case 'enviarAgregarVino':
+        $controller= new VinotecaController();
+        $controller->agregarVino(); 
+        break;
+    case 'eliminarCepa':
+        $controller= new VinotecaController();
+        $controller->eliminarCepa($params[1]); 
+        break;
+    case 'modificarCepa':
+        $controller= new VinotecaController();
+        $controller->showModificarCepa($params[1]); 
+        break;
+    case 'enviarModificarCepa':
+        $controller= new VinotecaController();
+        $controller->modificarCepa($params[1]); 
+        break;
+    case 'agregarCepa':
+        $controller= new VinotecaController();
+        $controller->showAgregarCepa(); 
+        break;
+    case 'enviarAgregarCepa':
+        $controller= new VinotecaController();
+        $controller->agregarCepa(); 
+        break;
+    case 'login':
+        $controller = new AuthController();
+        $controller->showLogin(); 
+        break;
+    case 'auth':
+        $controller = new AuthController();
+        $controller->auth();
+        break;
+    case 'logout':
+        $controller = new AuthController();
+        $controller->logout();
+        break;
+    default: 
+        $controller= new VinotecaController();
+        $controller->showHome();
+        break;
+
+}
