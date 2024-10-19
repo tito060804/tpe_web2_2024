35952993253316aa38afdb9cc64179d67945174d
tito060804 <144065294+tito060804@users.noreply.github.com>
2024-10-19 14:00:04 -0300

    Add files via upload
    todas las rutas funcionando

---
+++ tpe-web2-refactor/app/models/vinoteca.model.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once 'app/models/models.php';
+
+class VinotecaModel extends Model {
+
+    public function getVinos() {
+        $query = $this->db->prepare('SELECT a.*, b.id_cepa, b.Nombre_cepa FROM `db_vinos` a INNER JOIN `cepa` b ON a.id_cepa = b.id_cepa');
+        $query->execute();
+        $vinos = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $vinos;
+    }
+
+    public function getVino($id) {
+        $query= $this->db->prepare('SELECT a.*, b.id_cepa, b.Nombre_cepa,  FROM `db_vinos` a INNER JOIN `cepa` b ON a.id_cepa = b.id_cepa = c.id_bodega  WHERE `ID_vino` = ?');
+        $query->execute([$id]);
+        $vino= $query->fetch(PDO::FETCH_OBJ);
+
+        return $vino;
+    }
+
+    public function getCepa($id) {
+        $query= $this->db->prepare('SELECT * FROM `cepa`  WHERE `id_cepa` = ?');
+        $query->execute([$id]);
+        $cepa= $query->fetch(PDO::FETCH_OBJ);
+
+        return $cepa;
+    }
+
+    public function getCepas() {
+        $query= $this->db->prepare('SELECT id_cepa, Nombre_cepa FROM `cepa`');
+        $query->execute();
+        $cepas= $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $cepas;
+    }
+
+    public function getVinosPorCepa($id) {
+        $query= $this->db->prepare('SELECT a.*, b.id_bodega, b.Nombre_bodega FROM `vino` a INNER JOIN `bodega` b ON a.id_bodega = b.id_bodega  WHERE a.id_cepa = ?');
+        $query->execute([$id]);
+        $vinos= $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $vinos;
+    }
+
+    public function updateVino($Nombre, $Tipo, $Azucar, $id_cepa, $id) {    
+        $query = $this->db->prepare('UPDATE `vino` SET Nombre=?, Tipo=?, Azucar=?, id_cepa=? WHERE ID_vino = ?');
+        $query->execute([$Nombre, $Tipo, $Azucar, $id_cepa, $id]);
+    }
+
+    public function updateCepa($Nombre_cepa, $Aroma, $Maridaje, $Textura, $id) {    
+        $query = $this->db->prepare('UPDATE `cepa` SET Nombre_cepa=?, Aroma=?, Maridaje=?, Textura=? WHERE id_cepa = ?');
+        $query->execute([$Nombre_cepa, $Aroma, $Maridaje, $Textura, $id]);
+    }
+
+    public function deleteVino($id){
+        $query = $this->db->prepare('DELETE FROM `vino` WHERE ID_vino = ?');
+        $query->execute([$id]);
+    }
+
+    public function deleteCepa($id){
+        $query = $this->db->prepare('DELETE FROM `cepa` WHERE id_cepa = ?');
+        $query->execute([$id]);
+    }
+
+    public function insertVino($Nombre, $Tipo, $Azucar, $id_cepa){
+        $query = $this->db->prepare('INSERT INTO `vino` (Nombre, Tipo, Azucar, id_cepa) VALUES(?,?,?,?)');
+        $query->execute([$Nombre, $Tipo, $Azucar, $id_cepa]);
+
+        return $this->db->lastInsertId();
+    }
+
+    public function insertCepa($Nombre_cepa, $Aroma, $Maridaje, $Textura){
+        $query = $this->db->prepare('INSERT INTO `cepa` (Nombre_cepa, Aroma, Maridaje, Textura) VALUES(?,?,?,?)');
+        $query->execute([$Nombre_cepa, $Aroma, $Maridaje, $Textura]);
+
+        return $this->db->lastInsertId();
+    }
+}
---
+++ tpe-web2-refactor/database/db_vinoteca (2).sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2024 a las 19:05:14
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_vinoteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cepa`
+--
+
+CREATE TABLE `cepa` (
+  `id_cepa` int(11) NOT NULL,
+  `nombre_cepa` varchar(100) NOT NULL,
+  `aroma` varchar(45) DEFAULT NULL,
+  `maridaje` varchar(45) DEFAULT NULL,
+  `textura` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cepa`
+--
+
+INSERT INTO `cepa` (`id_cepa`, `nombre_cepa`, `aroma`, `maridaje`, `textura`) VALUES
+(1, 'Malbec', 'Presencia de frutos rojos como moras, frambue', 'pastas con salsas a base de tomate, carnes as', 'cuerpo importante y sabor persistente en boca'),
+(2, 'Syrah', 'Se caracteriza por un aroma afrutado que recu', 'Carnes rojas y de caza', 'En boca es fresco, de estructura media y text'),
+(3, 'Sauvignon blanc', 'Toques de grosellas, melón verde, pomelo o fr', 'Ideal para acompañar mariscos, carnes blancas', 'Son vinos secos, con poco cuerpo y con aroma '),
+(4, 'Merlot', 'Aromas a Frambuesas, guindas y especias como ', 'Ideal para acompañar pastas y pizzas', 'Se caracteriza por su textura suave en el pal'),
+(5, 'Chardonnay', 'Perfil cremoso con aroma a avellanas, miel y ', 'Acompañar con mariscos, pescado, carnes blanc', 'Es fresco, con una leve sensación de dulzura,');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `db_vinos`
+--
+
+CREATE TABLE `db_vinos` (
+  `id_vino` int(11) NOT NULL,
+  `nombre_vino` varchar(100) NOT NULL,
+  `cepa` int(11) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `db_vinos`
+--
+
+INSERT INTO `db_vinos` (`id_vino`, `nombre_vino`, `cepa`, `tipo`) VALUES
+(1, 'luigi bosca', 1, 'tinto'),
+(2, 'hermandad', 1, 'tinto'),
+(3, 'DON VALENTIN LACRADO', 3, 'vino blanco');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `password`) VALUES
+(1, 'webadmin', 'admin'),
+(2, 'tito@gmail.com', '1234');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cepa`
+--
+ALTER TABLE `cepa`
+  ADD PRIMARY KEY (`id_cepa`);
+
+--
+-- Indices de la tabla `db_vinos`
+--
+ALTER TABLE `db_vinos`
+  ADD PRIMARY KEY (`id_vino`),
+  ADD KEY `cepa` (`cepa`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cepa`
+--
+ALTER TABLE `cepa`
+  MODIFY `id_cepa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `db_vinos`
+--
+ALTER TABLE `db_vinos`
+  MODIFY `id_vino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `db_vinos`
+--
+ALTER TABLE `db_vinos`
+  ADD CONSTRAINT `db_vinos_ibfk_1` FOREIGN KEY (`cepa`) REFERENCES `cepa` (`id_cepa`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
